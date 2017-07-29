e7916a0c823ecf124d435aa7a2363a65918aac1d
Patrick Nicholls <padawagon@gmail.com>
2017-07-29 14:37:11 +1200

    Generalise base.html and create stub for map

--- libercrime/urls.py
+++ libercrime/urls.py
@@ -20,5 +20,6 @@ from main import views
 
 urlpatterns = [
     url(r'^$', views.home),
+    url(r'^$', views.map),
     url(r'^admin/', admin.site.urls),
 ]
--- main/views.py
+++ main/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'index.html')
+
+
+def map(request):
+    return render(request, 'map.html')
